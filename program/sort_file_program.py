41cce265488a350991e21e7971ff32fa2db106e5
ryouiti <5118037s@st.toho-u.ac.jp>
2021-09-29 22:27:33 +0900

    09-29　GUIの導入とテストデータの作成

--- program/sort_file_program.py
+++ program/sort_file_program.py
@@ -13,36 +13,50 @@ import os
 import re
 import shutil
 import sys
-def select_mode(option = sys.argv[0])->int:
-    option=option.lower()
-    if option =="-n":
-        mode  = 1
-    else:
-        mode = 2
-    return mode
+import tkinter.filedialog
+# def select_mode(option = sys.argv[0])->int:
+#     option=option.lower()
+#     if option =="-n":
+#         mode  = 1
+#     else:
+#         mode = 2
+#     return mode
 
-def select_dir(mode:int)->str:#ディレクトリをモードによって選ぶ
-    selectdir =""
-    if mode==1:
-        selectdir = os.path.dirname(__file__)
-    if mode==2:
-        selectdir =os.path.abspath(os.path.join(__file__,os.pardir,os.pardir))#os.pardirは親ディレクトリを参照する文字列 os.path.joinを組み合わせることで親ディレクトリが得られる
-    return selectdir
+# def select_dir_bymode(mode:int)->str:#ディレクトリをモードによって選ぶ
+#     nowdir =""
+#     if mode==1:
+#         nowdir = os.path.dirname(__file__)
+#     if mode==2:
+#         nowdir =os.path.abspath(os.path.join(__file__,os.pardir,os.pardir))#os.pardirは親ディレクトリを参照する文字列 os.path.joinを組み合わせることで親ディレクトリが得られる
+#     return nowdir
+
+
+def GUI_select_directory()->str:
+    iDir = os.path.abspath(os.path.dirname(__file__))
+    selected_file_path = tkinter.filedialog.askdirectory(initialdir = iDir)
+    return  selected_file_path
+
+def rogo_deployment(rogolist):
+    rogo_split=""
+    for rogo in rogolist:
+        rogo_split=rogo.replace(")("," ").split(" ")
+    return rogo_split
+
 
 ROGO_RE = re.compile(r"\((.*)\)")
 def sort_file_program():
-    selectdir = select_dir(select_mode())
-    for selectdir_list_name in os.listdir(selectdir):
+    user_selectdir = GUI_select_directory()
+    nowdir = user_selectdir
+    for selectdir_list_name in os.listdir(nowdir):
         if rogolist:=ROGO_RE.findall(selectdir_list_name):
-            for rogo in rogolist:
-                rogo_split = rogo.replace(")("," ").split(" ")
-                for rogo in rogo_split:
-                    rogo_abspath = os.path.join(selectdir,rogo)
-                    if not os.path.exists(rogo_abspath):
-                        os.makedirs(rogo_abspath)
-                    shutil.move(os.path.join(selectdir,selectdir_list_name),rogo_abspath)
-                    selectdir = os.path.join(selectdir,rogo)
-        selectdir = select_dir(select_mode())
+            rogo_split = rogo_deployment(rogolist)
+            for rogo in rogo_split:
+                rogo_abspath = os.path.join(nowdir,rogo)
+                if not os.path.exists(rogo_abspath):
+                    os.makedirs(rogo_abspath)
+                shutil.move(os.path.join(nowdir,selectdir_list_name),rogo_abspath)
+                nowdir = os.path.join(nowdir,rogo)
+        nowdir = user_selectdir
 
 if __name__=="__main__":
     sort_file_program()
