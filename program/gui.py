c03c6e31c1b352cd58973345008d45bfcae1b84c
ryouiti <5118037s@st.toho-u.ac.jp>
2021-09-29 23:15:08 +0900

    解像度の上昇

--- program/gui.py
+++ program/gui.py
@@ -1,6 +1,9 @@
 import os
 import tkinter.filedialog
+import ctypes
 def GUI_select_directory()->str:
+    ctypes.windll.shcore.SetProcessDpiAwareness(1)#DPIを調節してエクスプローラーの解像度を上げる  MAC環境では使えない 
     iDir = os.path.abspath(os.path.dirname(__file__))
     selected_file_path = tkinter.filedialog.askdirectory(initialdir = iDir)
     return  selected_file_path
+GUI_select_directory()
